955e06a252cc9d3deeefdcc5dd9a6f7d7e4c9a98
Briennaa <sdobson1995@gmail.com>
2019-08-09 19:41:47 -0400

    Added skin changing functionality to mug
    All skins will be held in Sprite objects under mug but only one will be active, PlayerPrefs.GetInt("MugSkin") ... 0 = Default

---
+++ Just a Mug/Assets/Scripts/MugSkins.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MugSkins : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int children = transform.childCount;
+        for (int i = 0; i < children; ++i)
+            if(transform.GetChild(i).name == PlayerPrefs.GetInt("MugSkin").ToString())
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+            }
+            else
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
